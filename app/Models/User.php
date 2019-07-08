72f7963fe85c86972214fbbf2b41669377e0dca0
corbin <771438815@qq.com>
2019-07-08 03:49:50 +0000

    消息通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,15 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Notifiable, MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -50,5 +55,18 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function notify($instance)
+    {
+        if($this->is == Auth::id)
+        {
+            return;
+        }
+        if(method_exists($instance, 'toDatabase'))
+        {
+            $this->increment('notification_count');
+        }
+        $this->laravelNotify($instance);
     }
 }
